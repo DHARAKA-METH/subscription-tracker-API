40849e18710e9457dd0e03400be59a71c5ca4831
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-03-23 22:26:23 +0530

    env update export  JWT_EXPIRES_IN,   JWT_COOKIE_EXPIRES_IN

--- config/env.js
+++ config/env.js
@@ -4,4 +4,11 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
 // Correct way to export environment variables
-export const {PORT,NODE_ENV,DB_URI} =process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  JWT_COOKIE_EXPIRES_IN,
+} = process.env;
