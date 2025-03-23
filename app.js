c21dadd589b7a1575ec3c8b82a9a5ea58312146f
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-03-23 21:17:06 +0530

    error middleware

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middlewares.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.end("Welcome to subscription tracker api");
